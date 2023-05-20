6c12decc8541fbb097c83948a74afe6d8ca59fb8
emayethossen <samir2021hossen@gmail.com>
2023-05-20 06:52:45 +0600

    specific data showing

--- src/components/MyToys/MyToys.jsx
+++ src/components/MyToys/MyToys.jsx
@@ -1,17 +1,21 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import MySingleToy from '../MySingleToy/MySingleToy';
 import { Helmet } from 'react-helmet';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const MyToys = () => {
-    const [toys, setToys] = useState([])
 
+    const { user } = useContext(AuthContext)
+
+    const [toys, setToys] = useState([])
+    const url = `http://localhost:5000/toys?email=${user.email}`
     useEffect(() => {
-        fetch('http://localhost:5000/toys')
+        fetch(url)
             .then(res => res.json())
             .then(data => {
                 setToys(data);
             })
-    }, [])
+    }, [url])
 
     return (
         <div className='w-3/4 mx-auto space-y-6 lg:mb-16 my-6'>
